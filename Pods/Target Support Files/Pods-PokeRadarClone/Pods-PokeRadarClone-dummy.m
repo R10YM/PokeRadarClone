e88f452ac4c6f908e611021c9255727df6708a11
Meng Yuan <mengyuan@Mengs-MacBook-Pro.local>
2016-12-11 00:34:14 -0500

    UI and Basic functions

---
+++ Pods/Target Support Files/Pods-PokeRadarClone/Pods-PokeRadarClone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokeRadarClone : NSObject
+@end
+@implementation PodsDummy_Pods_PokeRadarClone
+@end
---
+++ PokeRadarClone/PokeRadarClone-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinderClone-Bridging-Header.h
+//  PokeRadarClone
+//
+//  Created by Meng Yuan on 12/10/16.
+//  Copyright © 2016 Meng Yuan. All rights reserved.
+//
+
+#import "GeoFire.h"
